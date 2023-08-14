90d84a84bbb467c7e57695f58f21fef77c9d42ae
Rafael - RafilklDev <rafaelalmeidapenha@gmail.com>
2023-08-14 15:34:30 -0300

    Começando a elaborar tabela para pedido eletronico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@
 
 import { Formulario } from '@/components/form'
 import styles from './page.module.css'
+import { PedidoEletronico } from '@/components/pedidoEletronico'
 
 export default function Home() {
   return (
-    <Formulario />
+    <PedidoEletronico />
   )
 }
---
+++ src/components/pedidoEletronico.tsx
@@ -0,0 +1,14 @@
+export function PedidoEletronico() {
+    return <>
+        <table border={1}>
+            <tr>
+                <td height={100}>DOCE SABOR INDÚSTRIA E COMÉRCIO DE PRODUTOS ALIMENTÍCIOS EIRELI</td>
+            </tr>
+            <tr>
+                <td width={200}>CNPJ : 07.175.867/0002-08 - INSCRIÇÃO ESTADUAL :  143.154.297.110</td>
+                <td width={200}>CNPJ : 07.175.867/0002-08 - INSCRIÇÃO ESTADUAL :  143.154.297.110</td>
+                <td width={200}>CNPJ : 07.175.867/0002-08 - INSCRIÇÃO ESTADUAL :  143.154.297.110</td>
+            </tr>
+        </table>
+    </>
+}
